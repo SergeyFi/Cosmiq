c554081dbed9bf491a6fce3e29efe79976e09120
Sergey Firsov <zlofks@gmail.com>
2020-03-01 14:35:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cosmiq
+
+Developed with Unreal Engine 4
---
+++ Source/Cosmiq/CosmiqGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CosmiqGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COSMIQ_API ACosmiqGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
