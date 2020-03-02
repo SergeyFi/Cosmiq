a0e5c1c20bb1cb148175018527b62c2402da1c0a
Sergey Firsov <zlofks@gmail.com>
2020-03-02 22:29:18 +0300

    Add shield damage

---
+++ Source/Cosmiq/Objects/DamageTypes/ShieldDamage.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "ShieldDamage.generated.h"
+
+
+UCLASS()
+class COSMIQ_API UShieldDamage : public UDamageType
+{
+	GENERATED_BODY()
+	
+};
