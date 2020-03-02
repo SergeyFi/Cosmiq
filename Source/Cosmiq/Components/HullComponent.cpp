22a6edca7dc3c22deb36bb2ace9191964c211a10
Sergey Firsov <zlofks@gmail.com>
2020-03-02 15:19:33 +0300

    Setup hull component

--- Source/Cosmiq/Components/HullComponent.cpp
+++ Source/Cosmiq/Components/HullComponent.cpp
@@ -2,11 +2,16 @@
 
 
 #include "HullComponent.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UHullComponent::UHullComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+
+	HullCurrent = HullMax;
+
+	if (GetOwner()) GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHullComponent::OnOwnerTakeDamage);
 }
 
 
@@ -21,5 +26,33 @@ void UHullComponent::BeginPlay()
 void UHullComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+
+void UHullComponent::Heal(float Heal)
+{
+	HullCurrent += Heal;
+
+	if (HullCurrent > HullMax) HullCurrent = HullMax;
+}
+
+
+void UHullComponent::DamageHull(float Damage, AController* InstigatedBy)
+{
+	HullCurrent -= Damage;
+
+	if (FMath::IsNearlyZero(HullCurrent) || HullCurrent < 0.0f)
+	{
+		OnHealthEnded.Broadcast(InstigatedBy);
+		HullCurrent = 0.0f;
+	}
+}
+
+
+void UHullComponent::OnOwnerTakeDamage(
+	AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser
+)
+{
+	DamageHull(Damage, InstigatedBy);
 }
 
--- Source/Cosmiq/Components/HullComponent.h
+++ Source/Cosmiq/Components/HullComponent.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "HullComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FHealthEnded, AController*, Instigator);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class COSMIQ_API UHullComponent : public UActorComponent
@@ -20,9 +21,26 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hull component")
+	float HullCurrent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hull component")
+	float HullMax;
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	void Heal(float Heal);
+
+	void DamageHull(float Damage, AController* InstigatedBy);
+
+	UFUNCTION()
+		void OnOwnerTakeDamage(
+			class AActor* DamagedActor, float Damage, const class UDamageType* DamageType, 
+			class AController* InstigatedBy, class AActor* DamageCauser
+		);
+
+	UPROPERTY(BlueprintAssignable, Category = "Hull component")
+	FHealthEnded OnHealthEnded;
 };
