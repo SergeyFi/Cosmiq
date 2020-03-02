8bce4226ccef3c1b7e65f91b7fc6dc55118ed99b
Sergey Firsov <zlofks@gmail.com>
2020-03-02 22:22:34 +0300

    Add hull damage type

---
+++ Source/Cosmiq/Objects/DamageTypes/HullDamage.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "HullDamage.generated.h"
+
+
+UCLASS()
+class COSMIQ_API UHullDamage : public UDamageType
+{
+	GENERATED_BODY()
+	
+};
