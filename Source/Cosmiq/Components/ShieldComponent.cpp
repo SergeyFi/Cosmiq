eca4a2f1303cf8262451b2f7ecdf5fed0f563cf5
Sergey Firsov <zlofks@gmail.com>
2020-03-02 21:55:17 +0300

    Add base methods to Shield component

--- Source/Cosmiq/Components/ShieldComponent.cpp
+++ Source/Cosmiq/Components/ShieldComponent.cpp
@@ -2,12 +2,18 @@
 
 
 #include "ShieldComponent.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UShieldComponent::UShieldComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 
+	ShieldCurrent = ShieldMax;
+
+	if (GetOwner()) GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UShieldComponent::OnOwnerTakeDamage);
 }
 
 
@@ -15,15 +21,58 @@ UShieldComponent::UShieldComponent()
 void UShieldComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	
 }
 
+
+void UShieldComponent::StartRegeneration()
+{
+	UWorld* World = GetWorld();
+
+	if (World) World->GetTimerManager().SetTimer(RegenerationTimer, this, &UShieldComponent::Regeneration, RegenerationRate, true, RegenerationCooldown);
+}
+
+void UShieldComponent::Regeneration()
+{
+	Heal(RenenerationHeal);
+
+	if (ShieldCurrent == ShieldMax)
+	{
+		UWorld* World = GetWorld();
+
+		if (World) World->GetTimerManager().ClearTimer(RegenerationTimer);
+	}
+}
 
 // Called every frame
 void UShieldComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+}
+
+void UShieldComponent::Heal(float Heal)
+{
+	ShieldCurrent += Heal;
+
+	if (ShieldCurrent > ShieldMax) ShieldCurrent = ShieldMax;
+}
+
+void UShieldComponent::DamageShield(float Damage)
+{
+	ShieldCurrent -= Damage;
+
+	OnShieldDamaged.Broadcast();
+
+	if (ShieldCurrent < 0.0f || FMath::IsNearlyZero(ShieldCurrent))
+	{
+		ShieldCurrent = 0.0f;
+
+		OnShieldEnded.Broadcast();
+	}
+}
+
+void UShieldComponent::OnOwnerTakeDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+	DamageShield(Damage);
 }
 
--- Source/Cosmiq/Components/ShieldComponent.h
+++ Source/Cosmiq/Components/ShieldComponent.h
@@ -6,6 +6,7 @@
 #include "Components/SceneComponent.h"
 #include "ShieldComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FShieldDelegate);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class COSMIQ_API UShieldComponent : public USceneComponent
@@ -20,9 +21,46 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Shield")
+	float ShieldCurrent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Shield")
+	float ShieldMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Shield")
+	float RegenerationCooldown;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Shield")
+	float RegenerationRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Shield")
+	float RenenerationHeal;
+
+	FTimerHandle RegenerationTimer;
+
+	void StartRegeneration();
+
+	void Regeneration();
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void Heal(float Heal);
+
+	void DamageShield(float Damage);
+
+	UPROPERTY(BlueprintAssignable, Category = "Shild component")
+	FShieldDelegate OnShieldEnded;
+
+	UPROPERTY(BlueprintAssignable, Category = "Shild component")
+	FShieldDelegate OnShieldDamaged;
+
+	UFUNCTION()
+		void OnOwnerTakeDamage(
+			class AActor* DamagedActor, float Damage, const class UDamageType* DamageType,
+			class AController* InstigatedBy, class AActor* DamageCauser
+		);
 
 		
 };
