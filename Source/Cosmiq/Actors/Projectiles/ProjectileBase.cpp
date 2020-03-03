121b21477cf0faa1c38bbc43ddc36b338ec989fd
Sergey Firsov <zlofks@gmail.com>
2020-03-03 20:53:32 +0300

    Add base projectile methods

--- Source/Cosmiq/Actors/Projectiles/ProjectileBase.cpp
+++ Source/Cosmiq/Actors/Projectiles/ProjectileBase.cpp
@@ -3,10 +3,25 @@
 
 #include "ProjectileBase.h"
 
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+
+#include "Cosmiq/Objects/DamageTypes/ShieldDamage.h"
+#include "Cosmiq/Objects/DamageTypes/HullDamage.h"
+
+
+#include "Kismet/GameplayStatics.h"
+
 // Sets default values
 AProjectileBase::AProjectileBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
+	CollisionSphere->SetupAttachment(Mesh);
 
 }
 
@@ -17,10 +32,41 @@ void AProjectileBase::BeginPlay()
 	
 }
 
+void AProjectileBase::ProjectileMovement(float DeltaTime)
+{
+	AddActorLocalOffset(GetActorForwardVector() * DeltaTime * ProjectileSpeed);
+}
+
 // Called every frame
 void AProjectileBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	ProjectileMovement(DeltaTime);
+}
+
+void AProjectileBase::OnSphereOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		if (ShieldDamage > 0.0f)
+		{
+			TSubclassOf<UShieldDamage> ShieldDamageType;
+			UGameplayStatics::ApplyDamage(OtherActor, ShieldDamage, GetInstigatorController(), this, ShieldDamageType);
+
+			ShieldDamage = 0.0f;
+		}
+
+		if (HullDamage > 0.0f)
+		{
+			if (Cast<UStaticMeshComponent>(OverlappedComp))
+			{
+				TSubclassOf<UHullDamage> HullDamageType;
+				UGameplayStatics::ApplyDamage(OtherActor, HullDamage, GetInstigatorController(), this, HullDamageType);
+
+				Destroy();
+			}
+		}
+	}
 }
 
--- Source/Cosmiq/Actors/Projectiles/ProjectileBase.h
+++ Source/Cosmiq/Actors/Projectiles/ProjectileBase.h
@@ -19,8 +19,28 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Projectile")
+	float HullDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Projectile")
+	float ShieldDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Projectile")
+	float ProjectileSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Projectile")
+	class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Projectile")
+	class USphereComponent* CollisionSphere;
+
+	void ProjectileMovement(float DeltaTime);
+
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION()
+	void OnSphereOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
