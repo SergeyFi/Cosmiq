23b8479ca640e4bdcaaedd73d1d1c73b6d8c4d38
Sergey Firsov <zlofks@gmail.com>
2020-03-06 22:16:26 +0300

    Add enemy ship class

---
+++ Source/Cosmiq/Actors/Pawns/BattleShip/Enemy/EnemyShip.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Cosmiq/Actors/Pawns/BattleShip/BattleShip.h"
+#include "EnemyShip.generated.h"
+
+
+UCLASS()
+class COSMIQ_API AEnemyShip : public ABattleShip
+{
+	GENERATED_BODY()
+	
+};
