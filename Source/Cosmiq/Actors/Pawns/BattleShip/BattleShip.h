2f8d6bce77c027adb41ab31be01039ac2a29d047
Sergey Firsov <zlofks@gmail.com>
2020-03-06 21:44:23 +0300

    Add battleship class

---
+++ Source/Cosmiq/Actors/Pawns/BattleShip/BattleShip.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Cosmiq/Actors/Pawns/BaseShip.h"
+#include "BattleShip.generated.h"
+
+
+UCLASS()
+class COSMIQ_API ABattleShip : public ABaseShip
+{
+	GENERATED_BODY()
+	
+};
